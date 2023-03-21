2c71d798b43c57ddfe65e1e5ca691e22ab18c6f7
qbox <jakubprzydatek99@gmail.com>
2023-03-21 23:41:50 +0100

    initial commit

---
+++ src/main/java/pl/service/event_reminder/EventReminderApplication.java
@@ -0,0 +1,13 @@
+package pl.service.event_reminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventReminderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventReminderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/service/event_reminder/EventReminderApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.service.event_reminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventReminderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
